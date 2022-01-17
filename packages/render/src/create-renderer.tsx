71e448ecc707a2a3b288d804db97664f3fc0d347
Jose Felix <jfelx@outlook.com>
2022-01-17 02:51:27 -0400

    feat: improve create-renderer types

--- packages/render/src/create-renderer.tsx
+++ packages/render/src/create-renderer.tsx
@@ -11,16 +11,16 @@ type Data<Database, Props> = {
   [K in keyof Props]: Props[K] | ((db: Database) => Props[K])
 }
 
-interface Options<Database, Props> {
+interface Options<Database, Props, ExtraProperties> {
   component: ComponentType<Props>
-  data: Data<Database, Props>
+  data: Data<Database, Props> & ExtraProperties
   renderFunction: typeof render
   wrapper?: RenderOptions["wrapper"]
 }
 
-export function createRenderer<Database extends TestingDatabase, Props = {}>(
+export function createRenderer<Database extends TestingDatabase, Props = {}, ExtraProperties = {}>(
   db: any,
-  options: Options<Database, Props>
+  options: Options<Database, Props, ExtraProperties>
 ) {
   const { component: Component, data, renderFunction } = options
 
@@ -33,17 +33,17 @@ export function createRenderer<Database extends TestingDatabase, Props = {}>(
     }
   })
 
-  return (overrides: Record<keyof typeof props, any> = {}) => {
+  return (overrides: Partial<typeof options.data> = {}) => {
     const propsWithOverrides = {
       ...props,
       ...overrides,
-    } as Props
+    } as Props & ExtraProperties
 
     const renderUtils = renderFunction(<Component {...propsWithOverrides} />, { wrapper: options.wrapper })
 
     return {
       ...propsWithOverrides,
-      ...renderUtils,
+      renderUtils,
     }
   }
 }
